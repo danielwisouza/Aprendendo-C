2bd58df49dada9f92df9e5898370037a02d1988e
Daniel Willians Ignacio de Souza <daniel2wis@gmail.com>
2018-03-27 23:00:35 -0300

    Update Files

--- Lista de Exercicios 3 - Programando em C/Exercicios/Exercicio1/caracteres.c
+++ Lista de Exercicios 3 - Programando em C/Exercicios/Exercicio1/caracteres.c
@@ -23,3 +23,38 @@ void substitui_caracter(char s[],char c)
   printf("\nnovo ficou:%s",s);
 }
 
+//Tabela asci
+//97  - a
+//101 - e
+//105 - i
+//111 - o
+//117 - u
+int conta_vogais (char s[])
+{
+    int i, qtde = 0;
+    for(i=0;s[i]!='\0';i++)
+  {
+      if((tolower(s[i])==97) || (tolower(s[i]) == 101) || (tolower(s[i]) == 105) || (tolower(s[i]) == 111) || (tolower(s[i]) == 117)) qtde++;
+  }
+  return qtde;
+}
+
+void substitui_vogais_por_asterisco(char s[],char c)
+{
+  int i;
+  for(i=0;s[i]!='\0';i++)
+  {
+      if((tolower(s[i])==97) || (tolower(s[i]) == 101) || (tolower(s[i]) == 105) || (tolower(s[i]) == 111) || (tolower(s[i]) == 117)) s[i]='*';
+  }
+  printf("\nSubstituir Vogais Ficou:%s",s);
+}
+
+void conta_consoante (char s[])
+{
+  int i, qtde = 0;
+  for(i=0;s[i]!='\0';i++)
+  {
+      if((tolower(s[i])!=97) || (tolower(s[i]) != 101) || (tolower(s[i]) != 105) || (tolower(s[i]) != 111) || (tolower(s[i]) != 117)) qtde++;
+  }
+  printf("\nExistem %s consoantes no nome ",qtde);
+}
--- Lista de Exercicios 3 - Programando em C/Exercicios/Exercicio1/main.c
+++ Lista de Exercicios 3 - Programando em C/Exercicios/Exercicio1/main.c
@@ -15,7 +15,14 @@ int main()
     }
     printf("qual letra?");
     letra=getche();
-    printf("\n%d",conta_caracter(s[0],letra));
     substitui_caracter(s[1],letra);
+    printf("\n%d",conta_caracter(s[0],letra));
+    printf("\nVogais nome 1: %i",conta_vogais(s[0]));
+    printf("\nVogais nome 2: %i",conta_vogais(s[1]));
+    printf("\nVogais nome 3: %i",conta_vogais(s[2]));
+    substitui_vogais_por_asterisco(s[0],letra);
+    substitui_vogais_por_asterisco(s[1],letra);
+    substitui_vogais_por_asterisco(s[2],letra);
+    conta_consoante(s[0]);
     return 0;
 }
