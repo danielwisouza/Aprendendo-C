bd8485492179379f5799f750ce7644eb1101fc4b
Daniel Willians Ignacio de Souza <daniel2wis@gmail.com>
2018-05-05 01:46:25 -0300

    Menu simples exe2 4

--- Lista de Exercicios 4 - Programando em C/Exercicio2/main.c
+++ Lista de Exercicios 4 - Programando em C/Exercicio2/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    char op;
+    do{
+    printf("MENU DE OPCOES\n");
+    printf("a - Cadastrar uma pessoa\n");
+    printf("b - Calcular a idade de uma pessoa selecionada pelo usuário\n");
+    printf("c - Listar pessoas que nasceram a partir de um determinado ano\n");
+    printf("d - Listar pessoas que nasceram antes de um determinado ano\n");
+    printf("e-  Encontrar o nome de uma pessoa com determinado RG\n");
+    printf("s-  Sair\n");
+    printf("Escolha opcao\n");
+    op=getche();
+    op=tolower(op);
+    }while (op='s');
 }
